e96f98a83d75f8c431427b38801d01d434edbbaa
Diego Romero <diegoromeropersonal@gmail.com>
2021-06-08 22:16:07 -0300

    convert to hash router

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import styled from "styled-components";
 
 import Homepage from "./Homepage";
